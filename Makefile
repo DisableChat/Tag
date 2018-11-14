c0d132c35aa6e7e85be748431a49c3d6662be0d4
MagikarpUsedFly <wr140815@ohio.edu>
2018-11-14 01:54:35 -0500

    added makefile

---
+++ Makefile
@@ -0,0 +1,4 @@
+all: build
+
+build: tag.py mechanic.py draw.py
+	python3 tag.py
--- tag.py
+++ tag.py
@@ -1,5 +1,6 @@
 import curses
 from curses import wrapper
+import sys
 import mechanic
 import draw
 
@@ -22,40 +23,45 @@ def main(stdscr):
     Y2_Cor =    7
     X2_Cor =    8
 
+    try:
+        # Loop to continually update Y_Cor and X_Cor, then clear the screen and
+        # print the ascii symobl in correct location.
+        while True:
 
-    # Loop to continually update Y_Cor and X_Cor, then clear the screen and
-    # print the ascii symobl in correct location.
-    while True:
-
-        draw.printLoc(stdscr, Y_Cor, X_Cor, 0, 0)
-        draw.printLoc(stdscr, Y2_Cor, X2_Cor, 1, 0)
-        draw.printPlayerMove(stdscr, move, 2, 0)
-
-
-        draw.printPlayer(stdscr, playerOne, Y_Cor, X_Cor)
-        draw.printPlayer(stdscr, playerTwo, Y2_Cor, X2_Cor)
+            draw.printLoc(stdscr, Y_Cor, X_Cor, 0, 0)
+            draw.printLoc(stdscr, Y2_Cor, X2_Cor, 1, 0)
+            draw.printPlayerMove(stdscr, move, 2, 0)
 
 
-        if move == 1:
-            if mechanic.tag(Y_Cor, Y2_Cor, X_Cor, X2_Cor):
-                wrapper(draw.winner)
-            playerOne_y, playerOne_x = mechanic.GPDI_P1(Y_Cor, X_Cor, stdscr.getkey())
-            if not mechanic.tag(playerOne_y, Y_Cor, playerOne_x, X_Cor):
-                X_Cor = playerOne_x
-                Y_Cor = playerOne_y
-                move = 2
+            draw.printPlayer(stdscr, playerOne, Y_Cor, X_Cor)
+            draw.printPlayer(stdscr, playerTwo, Y2_Cor, X2_Cor)
 
-        elif move == 2:
-            if mechanic.tag(Y_Cor, Y2_Cor, X_Cor, X2_Cor):
-                wrapper(draw.winner)
-            playerTwo_y, playerTwo_x = mechanic.GPDI_P2(Y2_Cor, X2_Cor, stdscr.getkey())
-            if not mechanic.tag(playerTwo_y, Y2_Cor, playerTwo_x, X2_Cor):
-                X2_Cor = playerTwo_x
-                Y2_Cor = playerTwo_y
-                move = 1
 
-        stdscr.clear()
-        stdscr.refresh()
+            if move == 1:
+                if mechanic.tag(Y_Cor, Y2_Cor, X_Cor, X2_Cor):
+                    wrapper(draw.winner)
+                playerOne_y, playerOne_x = mechanic.GPDI_P1(Y_Cor, X_Cor, stdscr.getkey())
+                if not mechanic.tag(playerOne_y, Y_Cor, playerOne_x, X_Cor):
+                    X_Cor = playerOne_x
+                    Y_Cor = playerOne_y
+                    move = 2
+
+            elif move == 2:
+                if mechanic.tag(Y_Cor, Y2_Cor, X_Cor, X2_Cor):
+                    wrapper(draw.winner)
+                playerTwo_y, playerTwo_x = mechanic.GPDI_P2(Y2_Cor, X2_Cor, stdscr.getkey())
+                if not mechanic.tag(playerTwo_y, Y2_Cor, playerTwo_x, X2_Cor):
+                    X2_Cor = playerTwo_x
+                    Y2_Cor = playerTwo_y
+                    move = 1
+
+            stdscr.clear()
+            stdscr.refresh()
+    except KeyboardInterrupt:
+        sys.exit("Keyboard Interrupt, Quitting...")
+    except error as e:
+        print(e)
+
 
 
 
