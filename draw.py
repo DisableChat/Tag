a4a574c2375c3aa76ba0d21196fb5e46cd394415
MagikarpUsedFly <wr140815@ohio.edu>
2018-11-22 01:32:13 -0500

    added outer bounds visual and updated commenting for draw.py, mechanic.py and tag.py

--- draw.py
+++ draw.py
@@ -1,12 +1,16 @@
-#draw
+### draw for Tag.py ###
 import curses
 
-# Winner function just displays a user has been tagged
+##
+# Func:         winner
+# Param:        stdscn: None, curses screen var
+# Description:  winner function displays winner of tag game
+#               (called when someone "tags" a player)
+##
 def winner(stdscr: None) -> None:
     # Hide Cursor
     curses.curs_set(0)
     while True:
-
         stdscr.clear()
         for i in range(10):
             stdscr.addstr(i, 0, 'TAG!')
@@ -15,7 +19,11 @@ def winner(stdscr: None) -> None:
 
         stdscr.refresh()
 
-
+##
+# Func:         scrnSetup
+# Param:        stdscn: None, curses screen var
+# Description:  sets up colors and hides cursor
+##
 def scrnSetup(stdscr: None) -> None:
     # Hide Cursor
     curses.curs_set(0)
@@ -26,12 +34,37 @@ def scrnSetup(stdscr: None) -> None:
 
     stdscr.bkgd(curses.color_pair(yellow_background))
 
+##
+# Func:         printLoc
+# Param:        stdscrn: None, curses screen var
+#               CorY: int, y cordinate (current for player)
+#               Corx: int, x cordinate (current for player)
+#               startLoc_y: int, starting y location for player
+#               startLoc_x: int, starting x location for player
+# Description:  Prints the location of a player
+##
 def printLoc(stdscr, CorY: int, CorX: int, startLoc_y: int, startLoc_x: int) -> None:
     stdscr.addstr(startLoc_y, startLoc_x, 'Y_Cor:{} X_Cor:{}'.format(CorY, CorX))
 
+##
+# Func:         printLoc
+# Param:        stdscrn: None, curses screen var
+#               move: int, player turn value (1 for player 1, 2 for player 2)
+#               startLoc_y: int, starting y location for text
+#               startLoc_x: int, starting x location for text
+# Description:  Prints which player's turn it is
+##
 def printPlayerMove(stdscr, move: int, startLoc_y: int, startLoc_x: int) -> None:
     stdscr.addstr(startLoc_y, startLoc_x, 'Player Move:{}'.format(move))
 
+##
+# Func:         printPlayer
+# Param:        stdscn: None, curses screen var
+#               Player: int, player value (1 or 2)
+#               CorY: int, y cordinate (current for player)
+#               Corx: int, x cordinate (current for player)
+# Description:  prints the player piece and their corosponding color
+##
 def printPlayer(stdscr, Player: int, CorY: int, CorX: int) -> None:
 
     # Hide Cursor
@@ -47,3 +80,20 @@ def printPlayer(stdscr, Player: int, CorY: int, CorX: int) -> None:
         stdscr.addch(CorY, CorX, '@', curses.color_pair(cyan_dots))
     else:
         stdscr.addch(CorY, CorX, '@', curses.color_pair(default))
+
+##
+# Func:         printBoundaries
+# Param:        stdscn: None, curses screen var
+#               gridSize_Y: int, y value for boundary length
+#               gridSize_X: int, x value for boundary length
+# Description:  prints the boundries of the game
+##
+def printBoundaries(stdscr: None, gridSize_X: int, gridSize_Y: int) -> None:
+
+    for i in range(gridSize_X + 1):
+        stdscr.addstr(0, i, '#')
+        stdscr.addstr(gridSize_Y, i, '#')
+
+    for k in range(gridSize_Y):
+        stdscr.addstr(k, 0, '#')
+        stdscr.addstr(k, gridSize_X, '#')
